885bf4d960b8e6878f51a1680c633834b8086553
Rounik Jaiswal <rounik.jaiswal@bain.com>
2026-03-19 12:26:51 +0530

    Created recommended section in homepage

--- src/component/HomePage/BottomSection.tsx
+++ src/component/HomePage/BottomSection.tsx
@@ -1,6 +1,10 @@
 export function BottomSection(){
     return (
-        <div>
+        <div className="h-full w-full min-h-0 min-w-0 overflow-hidden">
+            <div className="w-full text-lg font-bold flex ">
+                <img className="pr-2" src="src\assets\Recommended_logo.svg" alt="Recommended logo" />
+                <div className="w-full min-w-0 shrink-0">Recommended actions</div>
+            </div>
             
         </div>
     )
---
+++ src/component/common/HomeRecommended.tsx
@@ -0,0 +1,5 @@
+export function HomeRecommended(){
+    return(
+        <div></div>
+    )
+}
